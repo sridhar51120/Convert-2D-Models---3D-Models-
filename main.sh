1577023804b8f9d72c9f389d414d002d5ab7700a
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-08-23 21:34:39 +0530

    initial commit

--- main.sh
+++
@@ -1,5 +0,0 @@
-!git clone https://github.com/facebookresearch/pifuhd
-!git clone https://github.com/Daniil-Osokin/lightweight-human-pose-estimation.pytorch.git
-!wget https://download.01.org/opencv/openvino_training_extensions/models/human_pose_estimation/checkpoint_iter_370000.pth
-
-# !sh ./scripts/download_trained_model.sh --> PreTrained  model
